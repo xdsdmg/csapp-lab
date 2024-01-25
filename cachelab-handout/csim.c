2a2af84b052b3e7f797f307dc515720cfa05d57e
xuedishangdemeigui <2216336843@qq.com>
2024-01-25 07:27:53 -1000

    cache lab

--- cachelab-handout/csim.c
+++ cachelab-handout/csim.c
@@ -7,6 +7,7 @@
 #include <stdint.h>
 
 #define K 64
+#define FILE_NAME_LEN_MAX 20
 
 struct cache_line
 {
@@ -23,26 +24,49 @@ struct cache_line new_cache_line(int s, int b)
     return cl;
 }
 
-int main(int argc, char *argv[])
+void parse_command_line_arg(int argc, char *argv[], int *s, int *E, int *b, int *t, char *file_name, bool *v)
 {
-    int s = 0, E = 0, b = 0, v = 0, t = 0;
-    char *file_name;
-
-    // Parse command line args.
+    *v = false;
     for (int i = 0; i < argc; i++)
     {
         if (strcmp(argv[i], "-s") == 0 && ++i < argc)
-            s = atoi(argv[i]);
+            *s = atoi(argv[i]);
         else if (strcmp(argv[i], "-E") == 0 && ++i < argc)
-            E = atoi(argv[i]);
+            *E = atoi(argv[i]);
         else if (strcmp(argv[i], "-b") == 0 && ++i < argc)
-            b = atoi(argv[i]);
+            *b = atoi(argv[i]);
         else if (strcmp(argv[i], "-t") == 0 && ++i < argc)
-            file_name = argv[i];
+        {
+            for (int n = 0; n < FILE_NAME_LEN_MAX && argv[i][n] != '\0'; n++)
+                file_name[n] = argv[i][n];
+        }
         else if (strcmp(argv[i], "-v") == 0)
-            v = 1;
+            *v = true;
     }
-    t = K - s - b;
+    *t = K - *s - *b;
+}
+
+void parse_trace_line(char line[100], int s, int b, char *op, uint64_t *addr, int *size, uint64_t *tag, uint64_t *set)
+{
+    char addr_[16] = {0}; // Address is 64-bit, need 16 Bytes (one char represents one hex digit).
+    int end = 3;
+    for (; line[end] != ','; end++)
+        ;
+    *op = line[1];
+    *size = atoi(&line[end + 1]);
+    strncat(addr_, line + 3, end - 3);
+    sscanf(addr_, "%lx", addr);
+    *tag = *addr >> (s + b);
+    *set = (*addr >> b) & ((1 << s) - 1);
+}
+
+int main(int argc, char *argv[])
+{
+    int s, E, b, t;
+    char file_name[FILE_NAME_LEN_MAX];
+    bool v;
+    parse_command_line_arg(argc, argv, &s, &E, &b, &t, file_name, &v);
+
     printf("s: %d, E: %d, b: %d, v: %d, t: %d, file_name: %s\n", s, E, b, v, t, file_name);
 
     // Init cache.
@@ -54,29 +78,115 @@ int main(int argc, char *argv[])
     // Parse trace file.
     FILE *file = fopen(file_name, "r");
     char line[100];
-    while (fgets(line, sizeof(line), file))
+    for (int clock = 0; fgets(line, sizeof(line), file); clock++)
     {
         if (line[0] != 32)
             continue;
 
-        char op = line[1], addr_[16] = {0}; // Address is 64-bit, need 16 Bytes (one char represents one hex digit).
-        int size;
-
-        int end = 3;
-        for (; line[end] != ','; end++)
-            ;
-
-        strncat(addr_, line + 3, end - 3);
-        size = atoi(&line[end + 1]);
-
         printf("line: %s", line);
 
-        uint64_t addr;
-        sscanf(addr_, "%lx", &addr);
-        uint64_t tag = addr >> (s + b);
-        uint64_t set = (addr >> b) & ((1 << s) - 1);
+        int size;
+        uint64_t addr, set, tag;
+        char op;
+        parse_trace_line(line, s, b, &op, &addr, &size, &tag, &set);
 
-        printf("op: %c, addr_: %s, size: %d, addr: %lX, set: %lX, tag: %lX\n", op, addr_, size, addr, set, tag);
+        int base = set * E;
+        bool hit = false, miss = false, eviction = false;
+        if (op == 'L')
+        {
+            for (int i = 0; i < E; i++)
+            {
+                if (cache[base + i].is_valid && cache[base + i].tag == tag)
+                {
+                    hit = true;
+                    cache[base + i].timestamp = clock;
+                    break;
+                }
+            }
+            if (!hit)
+            {
+                miss = true;
+                int i = 0;
+                for (; i < E; i++)
+                {
+                    if (!cache[base + i].is_valid)
+                    {
+                        cache[base + i].is_valid = true;
+                        cache[base + i].timestamp = clock;
+                        break;
+                    }
+                }
+                if (i == E)
+                {
+                    eviction = true;
+                    int index = 0, min_clock = cache[base].timestamp;
+                    for (int i = 1; i < E; i++)
+                    {
+                        if (cache[base + i].timestamp < min_clock)
+                        {
+                            index = i;
+                            min_clock = cache[base + i].timestamp;
+                        }
+                    }
+                    cache[base + index].timestamp = clock;
+                }
+            }
+        }
+        else if (op == 'M')
+        {
+            for (int i = 0; i < E; i++)
+            {
+                if (cache[base + i].is_valid && cache[base + i].tag == tag)
+                {
+                    hit = true;
+                    cache[base + i].timestamp = clock;
+                    break;
+                }
+            }
+            if (!hit)
+            {
+                miss = true;
+                int i = 0;
+                for (; i < E; i++)
+                {
+                    if (!cache[base + i].is_valid)
+                    {
+                        cache[base + i].is_valid = true;
+                        cache[base + i].timestamp = clock;
+                        break;
+                    }
+                }
+                if (i == E)
+                {
+                    eviction = true;
+                    int index = 0, min_clock = cache[base].timestamp;
+                    for (int i = 1; i < E; i++)
+                    {
+                        if (cache[base + i].timestamp < min_clock)
+                        {
+                            index = i;
+                            min_clock = cache[base + i].timestamp;
+                        }
+                    }
+                    cache[base + index].timestamp = clock;
+                }
+            }
+        }
+        else if (op == 'S')
+        {
+            for (int i = 0; i < E; i++)
+            {
+                if (cache[base + i].is_valid && cache[base + i].tag == tag)
+                {
+                    hit = true;
+                    cache[base + i].timestamp = clock;
+                    break;
+                }
+            }
+        }
+
+        printf("op: %c, size: %d, addr: %lX, set: %lX, tag: %lX\n", op, size, addr, set, tag);
+        printf("hit: %d, miss: %d, eviction: %d\n", hit, miss, eviction);
     }
     fclose(file);
 
